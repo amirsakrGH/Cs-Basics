68c221a3a1fe3e2d7f1a587caf1985f465ced33d
amirsakrGH <amirsakr001@gmail.com>
2024-12-13 02:21:37 +0200

    Assignment

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -7,28 +7,28 @@
 
 
             #region Q01
-            Console.WriteLine("Question01");
-            Console.Write("Enter a number: ");
-            int number = int.Parse(Console.ReadLine());
-            Console.WriteLine(number);
+            //Console.WriteLine("Question01");
+            //Console.Write("Enter a number: ");
+            //int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine(number);
             #endregion
 
             #region Q02
             //Value Type
-            Console.WriteLine("\n----------------------------------------------------------------------------\n");
-            Console.WriteLine("Question02");
-            short firstNumber = 15;
-            short secondNumber ;
+            //Console.WriteLine("\n----------------------------------------------------------------------------\n");
+            //Console.WriteLine("Question02");
+            //short firstNumber = 15;
+            //short secondNumber ;
 
-            secondNumber = firstNumber;
-            Console.WriteLine("firstNumber: {0}",firstNumber);
-            Console.WriteLine("secondNumber: {0}", secondNumber);
+            //secondNumber = firstNumber;
+            //Console.WriteLine("firstNumber: {0}",firstNumber);
+            //Console.WriteLine("secondNumber: {0}", secondNumber);
 
-            ++firstNumber;
-            Console.WriteLine("\nAfter Modifying\n");
+            //++firstNumber;
+            //Console.WriteLine("\nAfter Modifying\n");
             
-            Console.WriteLine("firstNumber: {0}", firstNumber);
-            Console.WriteLine("secondNumber: {0}", secondNumber);
+            //Console.WriteLine("firstNumber: {0}", firstNumber);
+            //Console.WriteLine("secondNumber: {0}", secondNumber);
             /*
              After incrementing firstNumber, no changes occur in secondNumber. 
              This happens because they are value types, meaning we are assigning the value of variables, 
@@ -38,29 +38,29 @@
 
             #region Q03
             //Reference Type
-            Console.WriteLine("\n----------------------------------------------------------------------------\n");
-            Console.WriteLine("Question02");
+            //Console.WriteLine("\n----------------------------------------------------------------------------\n");
+            //Console.WriteLine("Question02");
 
-            Point P01 = new Point();
-            Point P02 = new Point();
+            //Point P01 = new Point();
+            //Point P02 = new Point();
 
-            P01.x = 5;
-            P02.x = 10;
+            //P01.x = 5;
+            //P02.x = 10;
 
-            Console.WriteLine("P01.x: {0}\nP02.x: {1}", P01.x, P02.x);
+            //Console.WriteLine("P01.x: {0}\nP02.x: {1}", P01.x, P02.x);
 
 
-            P02 = P01;//they share the same memory address
+            //P02 = P01;//they share the same memory address
 
-            Console.WriteLine("After Assigning:");
-            Console.WriteLine("P01.x: {0}", P01.x);
-            Console.WriteLine("P02.x: {0}", P02.x);
+            //Console.WriteLine("After Assigning:");
+            //Console.WriteLine("P01.x: {0}", P01.x);
+            //Console.WriteLine("P02.x: {0}", P02.x);
 
-            P02.x = 45;
+            //P02.x = 45;
 
-            Console.WriteLine("After Modifying P02:");
-            Console.WriteLine("P01.x: {0}", P01.x);
-            Console.WriteLine("P02.x: {0}", P02.x);
+            //Console.WriteLine("After Modifying P02:");
+            //Console.WriteLine("P01.x: {0}", P01.x);
+            //Console.WriteLine("P02.x: {0}", P02.x);
 
             /*
              After assigning two reference variables, they point to the same memory location in the heap
